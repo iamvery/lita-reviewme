e8e2c4d47bc3db6478bb2bf5a6dc50f22ad48e3d
Jay Hayes <ur@iamvery.com>
2015-02-04 21:11:07 -0600

    Someone can remove a name from the reviewer list

--- lib/lita/handlers/reviewme.rb
+++ lib/lita/handlers/reviewme.rb
@@ -4,11 +4,18 @@ module Lita
       REDIS_LIST = "reviewers"
 
       route(/add (.+) to reviews/i, :add_reviewer, command: true, help: { "add @iamvery to reviews" => "adds @iamvery to the reviewer rotation" })
+      route(/remove (.+) from reviews/i, :remove_reviewer, command: true, help: { "remove @iamvery from reviews" => "removes @iamvery from the reviewer rotation" })
 
       def add_reviewer(response)
         reviewer = response.matches.flatten.first
         redis.rpush(REDIS_LIST, reviewer)
         response.reply("added #{reviewer} to reviews")
+      end
+
+      def remove_reviewer(response)
+        reviewer = response.matches.flatten.first
+        redis.lrem(REDIS_LIST, 0, reviewer)
+        response.reply("removed #{reviewer} from reviews")
       end
     end
 
--- spec/lita/handlers/reviewme_spec.rb
+++ spec/lita/handlers/reviewme_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe Lita::Handlers::Reviewme, lita_handler: true do
   it { routes_command("add @iamvery to reviews").to :add_reviewer }
+  it { routes_command("remove @iamvery from reviews").to :remove_reviewer }
 
   let(:reply) { replies.last }
 
@@ -10,6 +11,14 @@ describe Lita::Handlers::Reviewme, lita_handler: true do
       send_command("add @iamvery to reviews")
 
       expect(reply).to eq("added @iamvery to reviews")
+    end
+  end
+
+  describe "#remove_reviewer" do
+    it "removes a name from the list" do
+      send_command("remove @iamvery from reviews")
+
+      expect(reply).to eq("removed @iamvery from reviews")
     end
   end
 end
