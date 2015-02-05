ebf0da6432895512de8a053716f7b536ae7e5a85
Jay Hayes <ur@iamvery.com>
2015-02-04 21:10:22 -0600

    Someone can add name to reviewer list

--- lib/lita/handlers/reviewme.rb
+++ lib/lita/handlers/reviewme.rb
@@ -1,6 +1,15 @@
 module Lita
   module Handlers
     class Reviewme < Handler
+      REDIS_LIST = "reviewers"
+
+      route(/add (.+) to reviews/i, :add_reviewer, command: true, help: { "add @iamvery to reviews" => "adds @iamvery to the reviewer rotation" })
+
+      def add_reviewer(response)
+        reviewer = response.matches.flatten.first
+        redis.rpush(REDIS_LIST, reviewer)
+        response.reply("added #{reviewer} to reviews")
+      end
     end
 
     Lita.register_handler(Reviewme)
--- spec/lita/handlers/reviewme_spec.rb
+++ spec/lita/handlers/reviewme_spec.rb
@@ -1,4 +1,15 @@
 require "spec_helper"
 
 describe Lita::Handlers::Reviewme, lita_handler: true do
+  it { routes_command("add @iamvery to reviews").to :add_reviewer }
+
+  let(:reply) { replies.last }
+
+  describe "#add_reviewer" do
+    it "adds a name to the list" do
+      send_command("add @iamvery to reviews")
+
+      expect(reply).to eq("added @iamvery to reviews")
+    end
+  end
 end
